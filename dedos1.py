7c60a335329767354e00dc03605010be60f60e90
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2020-07-08 03:01:35 +0700

    Update and rename Dd.py to dedos1.py

---
+++ dedos1.py
@@ -0,0 +1,74 @@
+("clear")
+
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+
+import random
+import sys
+import time
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.2 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')
+mengetik('')                                                                                                                        mengetik('')
+mengetik('')
+mengetik(YY+'              .---.        .-----------N  ')
+mengetik('             /     \  __  /    ------U  ')
+mengetik('            / /     \(  )/    -----R  ')                                                                                  mengetik('           //////     \/ `   ---S ')
+mengetik('          //// / // :    : ---Y  ')                                                                                       mengetik('         // /   /  /`     --4  ')
+mengetik('        //          //..\\\H  ')
+mengetik('               ====UU====UU====       ')
+mengetik('               |    //||\\`    |       ')
+mengetik('               |      ```     |        ')
+mengetik('. N========A=======Z=======R=======I====L')
+mengetik('')
+mengetik('DEDOS DDOS DD')
+mengetik('Team : IMPERIOR T24M')                                                        mengetik('ANGGOTA : REMESTER/THIO/ZENTHER/HAREFA')
+mengetik('')
+mengetik('')
+mengetik(' Cara Menggunakan')
+mengetik('  python ddos.py -s ip-targetkalian -p 80 -t 135')
+mengetik('_contohh_')
+mengetik(' python2 ddos.py -s 27.0.0,1 -p 80 -t 135')
+
+mengetik('Masukan Setelah Pesan Berikut')
+mengetik(BB+'     SERANG BERSAMA ')
+mengetik('')
+
+os.system(' xdg-open https://api.whatsapp.com/send?phone=6281376441665&text=hallo%20adm>
